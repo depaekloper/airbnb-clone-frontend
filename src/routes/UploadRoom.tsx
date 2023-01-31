2a1e082ef1d9e66992a90005a088463c2fc6d85a
Seungwoo <depaekloper@gmail.com>
2023-02-01 08:18:16 +0900

    #21.0 Protected Pages

---
+++ src/routes/UploadRoom.tsx
@@ -0,0 +1,11 @@
+import useHostOnlyPage from "../components/HostOnlyPage";
+import ProtectedPage from "../components/ProtectedPage";
+
+export default function UploadRoom() {
+  useHostOnlyPage();
+  return (
+    <ProtectedPage>
+      <h1>upload</h1>
+    </ProtectedPage>
+  );
+}
